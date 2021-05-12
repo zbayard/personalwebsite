95cd06b970992471334851f05f05cc1cab3a5b5e
Zach <zbayard@gmail.com>
2021-05-12 12:28:23 -0400

    header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Particles from 'react-particles-js';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <NavBar/>
+    <>
+    <Particles
+      className='particles-canvas'
+      params={{
+        particles: {
+        number: {
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900
+              }
+            }
+          }
+        }
+      }
+    />
+      <NavBar/>
+      <Header/>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import Typed from 'react-typed'
+
+function Header(){
+    return(
+        <div className='header-wrapper'>
+            <div className='main-info'>
+                <h1>Web Development and websites promotion</h1>
+                <Typed
+                    className='typed-text'
+                    strings={["React", "Javascript", "Ruby on Rails", "PostgreSQL", "Redux"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href='#' className='btn-main-offer'>Contact Me</a>
+
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Header
