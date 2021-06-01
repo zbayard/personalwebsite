139bd4a24eb3cff19305b1caba596f71f9feba03
Zach <zbayard@gmail.com>
2021-06-01 13:45:38 -0400

    aboutme

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,12 @@ function AboutMe(){
         <div className="container py-5">
             <div className="row">
                 <div className="col-lg-6 col-xm-12">
-                    <img src={profile} alt="Zach Bayard"></img>    
+                    <div className="photo-wrap mb-5">
+                        <img className="profile-img" src={profile} alt="Zach Bayard"></img>    
+                    </div>
                 </div>
                 <div className="col-lg-6 col-xm-12">
-                    <h1>about me</h1>
+                    <h1 className="about-heading">about me</h1>
                     <p>
                         add a description here 
                     </p>
