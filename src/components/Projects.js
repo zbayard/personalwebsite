1ca0570be4171c36e55bc8d46c8e462b5fbe9b70
Zach <zbayard@gmail.com>
2021-06-07 11:35:52 -0400

    project hover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,11 @@ import React from 'react';
 import discourse from '../discourse.png';
 import gigbag from '../gigbag.png';
 import grooveplayer from '../grooveplayer.png';
+// FONT AWESOME IMPORTS
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faSearchPlus, faLaptopCode, faPlayCircle } from "@fortawesome/free-solid-svg-icons"
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
 
 
 function Projects(){
@@ -12,14 +17,20 @@ function Projects(){
                <div className="image-box-wrapper row justify-content-center">
                     <div className="project-image-box">
                         <img className="project-image" src={discourse} alt="Discourse"/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus}/> 
                     </div>
                     {/*  */}
                     <div className="project-image-box">
                         <img className="project-image" src={gigbag} alt="Gig Bag"/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus}/>
                     </div>
                     {/*  */}
                     <div className="project-image-box">
                         <img className="project-image" src={grooveplayer} alt="Groove Player"/>
+                        <div className="overflow"></div>
+                        <FontAwesomeIcon className="project-icon" icon={faSearchPlus}/>
                     </div>
                </div>
            </div>
