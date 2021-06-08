bba2699c15ea6c6b84aa7bb3e3fc59d76500d871
Zach <zbayard@gmail.com>
2021-06-08 13:37:51 -0400

    mobile fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ function NavBar(){
     return (
         <nav className={navbar ? "navbar navbar-expand-lg navbar-light bg-dark fixed-top" : "navbar navbar-expand-lg navbar-light fixed-top"} >
         <div className='container'>
-            <h2>zb</h2>
+            <Link smooth={true} to="home" className="nav-link" href="#"><h2>zb</h2></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{color: '#fff'}}/>
                 </button>
@@ -29,19 +29,19 @@ function NavBar(){
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <Link smooth={true} offset={-60} to="home" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+                        <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} offset={-60} to="about"  className="nav-link" href="#">about me</Link>
+                        <Link smooth={true} offset={-75} to="about"  className="nav-link" href="#">about me</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} offset={-60} to="projects" className="nav-link" href="#">projects</Link>
+                        <Link smooth={true} offset={-75} to="projects" className="nav-link" href="#">projects</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} offset={-60} to="skills" className="nav-link" href="#">toolbox & links</Link>
+                        <Link smooth={true} offset={-75} to="skills" className="nav-link" href="#">toolbox & links</Link>
                     </li>
                     <li className="nav-item">
-                        <Link smooth={true} offset={-60} to="contact" className="nav-link" href="#">contact</Link>
+                        <Link smooth={true} offset={-75} to="contact" className="nav-link" href="#">contact</Link>
                     </li>
         
                 </ul>
